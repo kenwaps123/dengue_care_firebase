54d46c5c859799ea4c0e0f8233a0790c8ce02c9d
kenwaps123 <b.canilanza.488740@umindanao.edu.ph>
2023-11-18 15:49:35 +0800

    brylle post edit

---
+++ lib/views/admins/admin_postedit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminEditPost extends StatefulWidget {
+  const AdminEditPost({super.key});
+
+  @override
+  State<AdminEditPost> createState() => _AdminEditPostState();
+}
+
+class _AdminEditPostState extends State<AdminEditPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
